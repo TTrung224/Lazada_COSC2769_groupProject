1fb7adf883b5fa9d92b1d6146661b46602a80961
s3877653 <hotranminhkhoi@gmail.com>
2023-08-16 23:20:42 +0700

    add css for seller product list

--- frontend/src/Component/Seller/SellerProductList.js
+++ frontend/src/Component/Seller/SellerProductList.js
@@ -1,18 +1,18 @@
 export default function ProductList(){
     return(
-        <>
+        <div className='seller-product-container'>
         <div className="seller-product-list">
                 <div className="seller-header-bar">
                     <button>Add more product</button>
                     <input type="search" placeholder='search for product'/>
                 </div>
                 <div className="product-item">
-                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ACat_img.jpg&psig=AOvVaw3Uy-qjuM6Bc4Q15nU-9Fui&ust=1692170995050000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCJCBs6uS3oADFQAAAAAdAAAAABAE"className="product-img"/>
-                    <div>
+                    <img src="https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180"className="product-img"/>
+                    <div className="product-item-middle">
                         <p>product name</p>
                         <p>product description</p>
                     </div>
-                    <div>
+                    <div className="product-item-right">
                         <p>product prize</p>
                         <p>product category</p>
                     </div>
@@ -22,6 +22,6 @@ export default function ProductList(){
                     </div>
                 </div>
         </div>
-        </>
+        </div>
     )
 }
