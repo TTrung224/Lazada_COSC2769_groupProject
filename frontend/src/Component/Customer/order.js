bcca0b64a7cc7232c1f1b3008e170f685fc734b6
Kho1Tr4n <s3916827@rmit.edu,vn>
2023-08-19 19:53:12 +0700

    Create order customer

---
+++ frontend/src/Component/Customer/order.js
@@ -0,0 +1,52 @@
+import React, {useContext, useEffect, useState} from 'react';
+import '../componentStyle.css';
+import Loader from '../Shared/loader';
+import { getItems } from './cartItemsApi';
+import { useLoaderData } from 'react-router-dom';
+import OrderItem from './orderItem';
+
+
+export async function loadItems(){
+    const orderList = await getItems();
+
+    return orderList
+}
+
+export default function Order(){
+    const orderList = useLoaderData()
+
+    const [isLoading, setIsLoading] = useState(false)
+
+
+    return(
+        <div class = "container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class = "col">
+                    <div class ="card">
+                        
+                        <div class ="card-body">
+                            <div class = "row">
+                                <div class = "col">
+                                        <h2>My Orders</h2>
+                                        <hr></hr>
+                                    
+                                    
+
+                                    {(isLoading) ? <Loader/> : <></>}
+                                    {cartList.map(product => <OrderItem key={product.id} product={product} />)}
+
+                                    
+                    
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+       
+    
+    )
+}
---
+++ frontend/src/Component/Customer/orderItem.js
@@ -0,0 +1,35 @@
+const orderItem = ({product}) => {
+    return ( 
+    <div class = "col">
+        <div class="row mb-4 d-flex justify-content-between align-items-center">
+            <div class="col-md-2 col-lg-2 col-xl-2">
+            <img
+                src={product.img}
+                class="img-fluid rounded-3" style ={{width:"100px"}}/>
+            </div>
+            
+            <div class="col-md-3 col-lg-3 col-xl-3">
+            <h6 class="text-black mb-0">{product.name}</h6>
+            </div>
+            
+            <input id="quantity" min="1" name="quantity" defaultValue= {product.quantity} type="number"
+                class="form-control form-control-sm" />
+
+
+            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                <h6 class="mb-0">${product.price}</h6>
+            </div>
+
+            <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                <button type ="button" class = "btn btn-success">Accept</button>
+                <button type ="button" class = "btn btn-danger">Deny</button>
+            </div>
+        </div>
+    </div>
+     );
+
+}
+
+
+ 
+export default cartItem;
