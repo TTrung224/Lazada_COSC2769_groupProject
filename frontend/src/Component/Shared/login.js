afa4c6b7c6b60b5fe44fef861c80663fffa38ccb
Trung Tran <s3891724@rmit.edu.vn>
2023-08-19 09:53:53 +0700

    no message

--- frontend/src/Component/Shared/login.js
+++ frontend/src/Component/Shared/login.js
@@ -1,13 +1,10 @@
 import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Context/loginSessionContext';
 import '../componentStyle.css';
 
 export default function Login(){
     const { authState: {isAuthenticated, user}} = useContext(AuthContext)
     const { loginFunc } = useContext(AuthContext)
-
-    const navigate = useNavigate()
 
     return(
         <div className='login-form'>
