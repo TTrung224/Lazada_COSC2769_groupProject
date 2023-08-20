29f025c65142124692791faf1c8ba50354f83a8b
s3877653 <hotranminhkhoi@gmail.com>
2023-08-20 20:20:12 +0700

    fix some css

--- frontend/src/Component/Shared/login.js
+++ frontend/src/Component/Shared/login.js
@@ -1,15 +1,26 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../Context/loginSessionContext';
 import '../componentStyle.css';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 
 export default function Login(){
     const { authState: {isAuthenticated, user}} = useContext(AuthContext)
     const { loginFunc } = useContext(AuthContext)
 
     return(
-        <div className='login-form'>
-            <button onClick={()=>loginFunc()}>login</button>
-            login form here
+        <div className='login-form'>           
+            <label for="login" ><h2>Login</h2></label>
+            <br/>
+            <div className='login-form' id="login">
+                <div className='signup-left'>
+                    <label for="email">Email</label>
+                    <input type='text' id="email" placeholder='Enter yopur email'/>
+                    <label for="password">Password</label>
+                    <input type='password' id='password' placeholder='Enter password' />
+                    <button type='button' onClick={()=>loginFunc()}>Login</button>
+                    <p>Dont have an account? <Link to='/signup'>Sign up</Link> here  </p>
+                </div>           
+        </div>
         </div>
     )
 }
