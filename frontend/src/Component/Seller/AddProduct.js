932c1bb10564b9d3ef2cc1807eab87bfa8189484
s3877653 <hotranminhkhoi@gmail.com>
2023-08-28 14:22:18 +0700

    fix UI

--- frontend/src/Component/Seller/AddProduct.js
+++ frontend/src/Component/Seller/AddProduct.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import Navbar from '../Shared/navbar';
+import { useState } from 'react';
+
 export default function AddProduct(){
 
     var categoryList = [
@@ -8,10 +10,12 @@ export default function AddProduct(){
         {id:"3",name:"cat3"}
     ]
 
-    var currenyCategory
+    const[currenyCategory,setCurrentCategory] = useState("null")
+     
 
     function handleInput(e){
-       currenyCategory = e.target.value
+        setCurrentCategory(e.target.value)
+       console.log(currenyCategory)
         
     }
     return(
@@ -35,42 +39,40 @@ export default function AddProduct(){
                         <input type='text' id="add-product-price" placeholder='add product price' />
                         <label for="add-product-category">Product category</label>
                         <select type='text' id="add-product-category" placeholder='add product category' onChange={event=>handleInput(event)}>
-                            <option value="#">Sellect</option>
+                            <option value="null">Sellect</option>
                             {categoryList.map(category =>{
                                 return(                      
                                     <option value={category.id}>{category.name}</option>                                                                                                     
                                 )
                             })}
                         </select>
-                        {categoryList.map(category=>(
+                       {currenyCategory ==="1"?(
                             <div className='category-attribute-input'>
-                                <label for="attribute1">{category.name} attribute</label>
-                                <input type="text"  id="attribute1" value="box1"/>
-                                <label for="attribute2">{category.name} attribute</label>
-                                <input type="text"  id="attribute2" value="box2"/>
+                                <label for="attribute1">cat1 attribute</label>
+                                <input type="text"  id="attribute1" placeholder='input attribute'/>
+                                <label for="attribute2">cat1 attribute</label>
+                                <input type="text"  id="attribute2" placeholder='input attribute'/>
                             </div>
-                        ))}
-                        
-                       {/*  */}
-
-                        
-                        {/* <div className='category-checkbox2'>
-                            <label for="cat2-box1">cat2</label>
-                            <input type="checkbox"  id="cat2-box1" value="box1"/>
-                            <label for="cat2-box2">cat2</label>
-                            <input type="checkbox"  id="cat2-box2" value="box2"/>
-                            <label for="cat2-box3">cat2</label>
-                            <input type="checkbox"  id="cat2-box3" value="box3"/>
-                        </div>
-                        <div className='category-checkbox3'>
-                            <label for="cat3-box1">cat3</label>
-                            <input type="checkbox"  id="cat3-box1" value="box1"/>
-                            <label for="cat3-box2">cat3</label>
-                            <input type="checkbox"  id="cat3-box2" value="box2"/>
-                            <label for="cat3-box3">cat3</label>
-                            <input type="checkbox"  id="cat3-box3" value="box3"/>
-                        </div> */}
-                        
+                       ):""}
+                      {currenyCategory ==="2"?(
+                            <div className='category-attribute-input'>
+                                <label for="attribute1">cat2 attribute</label>
+                                <input type="text"  id="attribute1" placeholder='input attribute'/>
+                                <label for="attribute2">cat2 attribute</label>
+                                <input type="text"  id="attribute2" placeholder='input attribute'/>
+                            </div>
+                       ):""}
+                       {currenyCategory ==="3"?(
+                            <div className='category-attribute-input'>
+                                <label for="attribute1">cat3 attribute</label>
+                                <input type="text"  id="attribute1" placeholder='input attribute'/>
+                                <label for="attribute2">cat3 attribute</label>
+                                <input type="text"  id="attribute2" placeholder='input attribute'/>
+                            </div>
+                       ):""}
+                            
+                     
+                                                                     
                     </div>     
                 </div>
                 <div className='product-add-button'>
