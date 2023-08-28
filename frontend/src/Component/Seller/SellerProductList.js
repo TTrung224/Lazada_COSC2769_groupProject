b26e97e8c8a9bc00e9dc0a38849d83aa53faf9c6
s3877653 <hotranminhkhoi@gmail.com>
2023-08-28 21:10:56 +0700

    add part of search

--- frontend/src/Component/Seller/SellerProductList.js
+++ frontend/src/Component/Seller/SellerProductList.js
@@ -3,11 +3,11 @@ import { useState,useRef } from 'react';
 export default function ProductList(){
 
     var productList = [
-        {id:"1",name:"fppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"7000000",category:"fruit,red",addedDate:"1/1/2012"},
-        {id:"2",name:"cppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"8000000",category:"fruit,red",addedDate:"1/2/2012"},
-        {id:"3",name:"dppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"5000000",category:"fruit,red",addedDate:"1/3/2012"},
-        {id:"4",name:"appple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"4000000",category:"fruit,red",addedDate:"1/4/2012"},
-        {id:"5",name:"eppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"9000000",category:"fruit,red",addedDate:"1/5/2012"},
+        {id:"1",name:"fppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:7000000,category:"fruit,red",addedDate:"1/1/2012"},
+        {id:"2",name:"cppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:8000000,category:"fruit,red",addedDate:"1/2/2012"},
+        {id:"3",name:"dppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:5000000,category:"fruit,red",addedDate:"1/3/2012"},
+        {id:"4",name:"appple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:4000000,category:"fruit,red",addedDate:"1/4/2012"},
+        {id:"5",name:"eppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:9000000,category:"fruit,red",addedDate:"1/5/2012"},
         {id:"6",name:"ippple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"2000000",category:"fruit,red",addedDate:"1/6/2012"},
         {id:"7",name:"dppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"1000000",category:"fruit,red",addedDate:"1/7/2012"},
         {id:"8",name:"wppple2",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"3000000",category:"fruit,red",addedDate:"1/1/2016"},
@@ -17,14 +17,20 @@ export default function ProductList(){
     
 
     const [products,setProduct] = useState(productList)
-    // const [currentSort,setCurrentSort] = useState("null")
+    const [search, setSearch] = useState("")
+
     
     const handleDelete =(id) =>{    
         setProduct(products => products.filter(product => product.id !== id))
         alert("Product id: "+id+" deleted")     
     }
-    var sortValue
-    var sortValue2
+    function handleSearch(product){
+        if(search.toLowerCase() ===""){
+            return product
+        }else if(product.name?.toLowerCase().includes(search) || String(product.price)?.includes(search) || product.addedDate?.includes(search)){
+            return  product
+        }
+    }
     var currentSort = useRef("")
     function handleSort(){
         if(currentSort.current.value === "name"){
@@ -54,8 +60,10 @@ export default function ProductList(){
                             <option value ="price">Price</option>
                             <option value="date">Added date</option>
                         </select>
+                        <input type="text" placeholder='search product' onChange={(e) =>setSearch(e.target.value)}/>
+                        <p>{search}</p>
                     </div>
-                    {products.map(product=>{
+                    {products.filter(product=>handleSearch(product)).map(product=>{
                         return(
                             <div className="product-item" key={product.id}>
                                 <img src={product.img} className="product-img" alt='product img'/>
@@ -66,6 +74,7 @@ export default function ProductList(){
                                 <div className="product-item-right">
                                     <p>Price: <b>{product.prize}</b></p>
                                     <p>Category: <b>{product.category}</b></p>
+                                    <p>Added Date: <b>{product.addedDate}</b></p>
                                 </div>
                                 <div className="seller-item-button">
                                     <Link className="item-edit" to="/seller/product/editproduct"><b>Edit</b></Link>
