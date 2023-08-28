6c893d50913f92e92797cb8c4be584af0226ee68
s3877653 <hotranminhkhoi@gmail.com>
2023-08-28 16:15:33 +0700

    add sort

--- frontend/src/Component/Seller/SellerProductList.js
+++ frontend/src/Component/Seller/SellerProductList.js
@@ -1,39 +1,44 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 export default function ProductList(){
 
     var productList = [
-        {id:"1",name:"fppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"7000000",category:"fruit,red",addedDate:9},
-        {id:"2",name:"cppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"8000000",category:"fruit,red",addedDate:8},
-        {id:"3",name:"dppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"5000000",category:"fruit,red",addedDate:7},
-        {id:"4",name:"appple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"4000000",category:"fruit,red",addedDate:6},
-        {id:"5",name:"eppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"9000000",category:"fruit,red",addedDate:5},
-        {id:"6",name:"ippple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"2000000",category:"fruit,red",addedDate:4},
-        {id:"7",name:"dppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"1000000",category:"fruit,red",addedDate:3},
-        {id:"8",name:"wppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"3000000",category:"fruit,red",addedDate:2},
-        {id:"9",name:"wppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"6000000",category:"fruit,red",addedDate:1}
+        {id:"1",name:"fppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"7000000",category:"fruit,red",addedDate:"1/1/2012"},
+        {id:"2",name:"cppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"8000000",category:"fruit,red",addedDate:"1/2/2012"},
+        {id:"3",name:"dppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"5000000",category:"fruit,red",addedDate:"1/3/2012"},
+        {id:"4",name:"appple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"4000000",category:"fruit,red",addedDate:"1/4/2012"},
+        {id:"5",name:"eppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"9000000",category:"fruit,red",addedDate:"1/5/2012"},
+        {id:"6",name:"ippple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"2000000",category:"fruit,red",addedDate:"1/6/2012"},
+        {id:"7",name:"dppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"1000000",category:"fruit,red",addedDate:"1/7/2012"},
+        {id:"8",name:"wppple2",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"3000000",category:"fruit,red",addedDate:"1/1/2016"},
+        {id:"9",name:"wppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"6000000",category:"fruit,red",addedDate:"1/1/2015"}
     ]
 
     
 
     const [products,setProduct] = useState(productList)
-
+    // const [currentSort,setCurrentSort] = useState("null")
+    
     const handleDelete =(id) =>{    
         setProduct(products => products.filter(product => product.id !== id))
         alert("Product id: "+id+" deleted")     
     }
-    function handleSort(e){
-        if(e.target.value ==="name"){
-            setProduct(products => products.sort((a,b)=> a.name > b.name ? 1 : -1))
-        }else if(e.target.value === "price"){
-            setProduct(products => products.sort((a,b)=> a.prize > b.prize ? 1 : -1))
-        }else if(e.target.value === "date"){
-            setProduct(products => products.sort((a,b)=> a.addedDate > b.addedDate ? 1 : -1))
+    var sortValue
+    var sortValue2
+    var currentSort = useRef("")
+    function handleSort(){
+        if(currentSort.current.value === "name"){
+            const newProducts = [...products].sort((a,b)=> a.name > b.name ? 1 : -1)
+            setProduct(newProducts)
+        }else if(currentSort.current.value === "price"){
+            const newProducts = [...products].sort((a,b)=> a.prize > b.prize ? 1 : -1)
+            setProduct(newProducts)
+        }else if(currentSort.current.value === "date"){
+            const newProducts = [...products].sort((a,b)=> a.addedDate.split('/').reverse().join().localeCompare(b.addedDate.split('/').reverse().join()))
+            setProduct(newProducts) 
         }else{
             setProduct(productList)
         }
-        console.log(e.target.value)
-        
     }
 
 
@@ -43,8 +48,8 @@ export default function ProductList(){
                     <div className="seller-header-bar">
                         <Link className='add-product' to="/seller/product/addproduct">Add more product</Link>
                         <label for="seller-sort">Sort by:</label>
-                        <select id="seller sort" className='seller-select-sort' onChange={(event)=>handleSort(event)}>
-                            <option value = "none">None</option>
+                        <select id="seller sort" className='seller-select-sort' ref={currentSort} onChange={()=>handleSort()}>
+                            <option value ="null">None</option>
                             <option value ="name">Name</option>
                             <option value ="price">Price</option>
                             <option value="date">Added date</option>
