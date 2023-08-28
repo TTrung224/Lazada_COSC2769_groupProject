513aa96266fd006da05336c8172c0ec3ea4124e5
s3877653 <hotranminhkhoi@gmail.com>
2023-08-29 00:36:26 +0700

    add filter except date

--- frontend/src/Component/Seller/SellerProductList.js
+++ frontend/src/Component/Seller/SellerProductList.js
@@ -8,16 +8,20 @@ export default function ProductList(){
         {id:"3",name:"dppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:5000000,category:"fruit,red",addedDate:"1/3/2012"},
         {id:"4",name:"appple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:4000000,category:"fruit,red",addedDate:"1/4/2012"},
         {id:"5",name:"eppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize: 9000000,category:"fruit,red",addedDate:"1/5/2012"},
-        {id:"6",name:"ippple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"2000000",category:"fruit,red",addedDate:"1/6/2012"},
-        {id:"7",name:"dppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"1000000",category:"fruit,red",addedDate:"1/7/2012"},
-        {id:"8",name:"wppple2",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"3000000",category:"fruit,red",addedDate:"1/1/2016"},
-        {id:"9",name:"wppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:"6000000",category:"fruit,red",addedDate:"1/1/2015"}
+        {id:"6",name:"ippple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:2000000,category:"fruit,red",addedDate:"1/6/2012"},
+        {id:"7",name:"dppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:1000000,category:"fruit,red",addedDate:"1/7/2012"},
+        {id:"8",name:"wppple2",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:3000000,category:"fruit,red",addedDate:"1/1/2016"},
+        {id:"9",name:"wppple",img:"https://tse4.mm.bing.net/th?id=OIP.YwA6xGkFItqFcdN8eHm-aAAAAA&pid=Api&P=0&h=180",description: "eifjghsfuighuifghfdffgrgergwerg",prize:6000000,category:"fruit,red",addedDate:"1/1/2015"}
     ]
 
     
 
     const [products,setProduct] = useState(productList)
     const [search, setSearch] = useState("")
+    const [minPrice,setMinPrice] = useState(0)
+    const [maxPrice,setMaxPrice] = useState(0)
+    const [minDate,setMinDate] = useState("")
+    const [maxDate,setMaxDate] = useState("")
 
     
     const handleDelete =(id) =>{    
@@ -25,12 +29,29 @@ export default function ProductList(){
         alert("Product id: "+id+" deleted")     
     }
     function handleSearch(product){
+       
         if(search.toLowerCase() ===""){
             return product
-        }else if(product.name?.toLowerCase().includes(search) || String(product.price)?.includes(search) || product.addedDate?.includes(search)){
+        }else if(product.name?.toLowerCase().includes(search) ){
             return  product
         }
     }
+
+    function handlePriceFilter(product){
+        if(product.prize >= Number.parseInt(minPrice) && product.prize <= Number.parseInt(maxPrice)){
+            return product
+        }else if(minPrice == "" && maxPrice == ""){
+            return product
+        }
+         }
+
+        function handleDateFilter(product){
+           if(product.addedDate >= minDate && product.addedDate <= maxDate){
+            return product
+           }else if(minDate === "" && maxDate ===""){
+            return product
+           }
+        }
     var currentSort = useRef("")
     function handleSort(){
         if(currentSort.current.value === "name"){
@@ -60,10 +81,22 @@ export default function ProductList(){
                             <option value ="price">Price</option>
                             <option value="date">Added date</option>
                         </select>
-                        <input type="text" placeholder='search product' onChange={(e) =>setSearch(e.target.value)}/>
-                        <p>{search}</p>
+                        <input type="text" placeholder='search by name' onChange={(e) =>setSearch(e.target.value)} className='product-name-search'/>
+                        <div className='seller-filter'>
+                            <p><b>Price:</b></p>
+                            <input type="number" placeholder='Min price'onChange={(e=>setMinPrice(e.target.value))}/>
+                            <input type="number" placeholder='Max price' onChange={(e=>setMaxPrice(e.target.value))}/>
+                        </div>
+                        <div className='seller-filter'>
+                            <p><b>Date:</b></p>
+                            <input type="number" placeholder='Min date' onChange={(e=>setMinDate(e.target.value))}/>
+                            <input type="number" placeholder='Max date' onChange={(e=>setMaxDate(e.target.value))}/>
+                        </div>
+                        {/*  */}
                     </div>
-                    {products.filter(product=>handleSearch(product)).map(product=>{
+                    {products.filter(product => handleSearch(product))
+                    .filter(product => handlePriceFilter(product))
+                    .filter(product => handleDateFilter(product)).map(product=>{
                         return(
                             <div className="product-item" key={product.id}>
                                 <img src={product.img} className="product-img" alt='product img'/>
